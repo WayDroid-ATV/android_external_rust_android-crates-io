ecf3fb17e085cab433b5a524c2963ead9d586948
James Farrell <jamesfarrell@google.com>
2024-11-07 22:48:59 +0000

    Update zerocopy and zerocopy-derive to 0.8.9
    Bug: 372549215
    Test: treehugger
    Change-Id: I508c16418c4b6e46bd24696f972769d254f8f3e2

--- crates/zerocopy-derive/tests/struct_known_layout.rs
+++ crates/zerocopy-derive/tests/struct_known_layout.rs
@@ -1,65 +1,61 @@
-// Copyright 2022 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2023 The Fuchsia Authors
+//
+// Licensed under a BSD-style license <LICENSE-BSD>, Apache License, Version 2.0
+// <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>, or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your option.
+// This file may not be copied, modified, or distributed except according to
+// those terms.
 
+// See comment in `include.rs` for why we disable the prelude.
+#![no_implicit_prelude]
 #![allow(warnings)]
 
-#[macro_use]
-mod util;
+include!("include.rs");
 
-use std::{marker::PhantomData, option::IntoIter};
-
-use {
-    static_assertions::assert_impl_all,
-    zerocopy::{DstLayout, KnownLayout},
-};
-
-use crate::util::AU16;
-
-#[derive(KnownLayout)]
+#[derive(imp::KnownLayout)]
 struct Zst;
 
-assert_impl_all!(Zst: KnownLayout);
+util_assert_impl_all!(Zst: imp::KnownLayout);
 
-#[derive(KnownLayout)]
+#[derive(imp::KnownLayout)]
 struct One {
     a: bool,
 }
 
-assert_impl_all!(One: KnownLayout);
+util_assert_impl_all!(One: imp::KnownLayout);
 
-#[derive(KnownLayout)]
+#[derive(imp::KnownLayout)]
 struct Two {
     a: bool,
     b: Zst,
 }
 
-assert_impl_all!(Two: KnownLayout);
+util_assert_impl_all!(Two: imp::KnownLayout);
 
-#[derive(KnownLayout)]
-struct TypeParams<'a, T, I: Iterator> {
+#[derive(imp::KnownLayout)]
+struct TypeParams<'a, T, I: imp::Iterator> {
     a: I::Item,
     b: u8,
-    c: PhantomData<&'a [u8]>,
-    d: PhantomData<&'static str>,
-    e: PhantomData<String>,
+    c: imp::PhantomData<&'a [::core::primitive::u8]>,
+    d: imp::PhantomData<&'static ::core::primitive::str>,
+    e: imp::PhantomData<imp::String>,
     f: T,
 }
 
-assert_impl_all!(TypeParams<'static, (), IntoIter<()>>: KnownLayout);
-assert_impl_all!(TypeParams<'static, AU16, IntoIter<()>>: KnownLayout);
+util_assert_impl_all!(TypeParams<'static, (), imp::IntoIter<()>>: imp::KnownLayout);
+util_assert_impl_all!(TypeParams<'static, util::AU16, imp::IntoIter<()>>: imp::KnownLayout);
 
 // Deriving `KnownLayout` should work if the struct has bounded parameters.
 
-#[derive(KnownLayout)]
+#[derive(imp::KnownLayout)]
 #[repr(C)]
-struct WithParams<'a: 'b, 'b: 'a, const N: usize, T: 'a + 'b + KnownLayout>(
+struct WithParams<'a: 'b, 'b: 'a, T: 'a + 'b + imp::KnownLayout, const N: usize>(
     [T; N],
-    PhantomData<&'a &'b ()>,
+    imp::PhantomData<&'a &'b ()>,
 )
 where
     'a: 'b,
     'b: 'a,
-    T: 'a + 'b + KnownLayout;
+    T: 'a + 'b + imp::KnownLayout;
 
-assert_impl_all!(WithParams<'static, 'static, 42, u8>: KnownLayout);
+util_assert_impl_all!(WithParams<'static, 'static, u8, 42>: imp::KnownLayout);
