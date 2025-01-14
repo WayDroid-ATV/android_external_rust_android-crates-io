943dc7c6883bad3459dbbd27e7fb4afe66e405d2
James Farrell <jamesfarrell@google.com>
2025-01-14 03:05:38 +0000

    Update libz-sys to 1.1.21
    Test: m
    Change-Id: I3d4faf08c9350f734cf9f971b179377c01715d3a

--- crates/libz-sys/src/zlib-ng/arch/x86/adler32_ssse3.c
+++ crates/libz-sys/src/zlib-ng/arch/x86/adler32_ssse3.c
@@ -6,15 +6,15 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-#include "../../zbuild.h"
-#include "../../adler32_p.h"
+#include "zbuild.h"
+#include "adler32_p.h"
 #include "adler32_ssse3_p.h"
 
-#ifdef X86_SSSE3_ADLER32
+#ifdef X86_SSSE3
 
 #include <immintrin.h>
 
-Z_INTERNAL uint32_t adler32_ssse3(uint32_t adler, const unsigned char *buf, size_t len) {
+Z_INTERNAL uint32_t adler32_ssse3(uint32_t adler, const uint8_t *buf, size_t len) {
     uint32_t sum2;
 
      /* split Adler-32 into component sums */
@@ -54,7 +54,7 @@ Z_INTERNAL uint32_t adler32_ssse3(uint32_t adler, const unsigned char *buf, size
         if (len < 16 + align_offset) {
             /* Let's eat the cost of this one unaligned load so that
              * we don't completely skip over the vectorization. Doing
-             * 16 bytes at a time unaligned is is better than 16 + <= 15
+             * 16 bytes at a time unaligned is better than 16 + <= 15
              * sums */
             vbuf = _mm_loadu_si128((__m128i*)buf);
             len -= 16;
--- crates/libz-sys/src/zlib-ng/arch/x86/chunkset_sse2.c
+++ crates/libz-sys/src/zlib-ng/arch/x86/chunkset_sse2.c
@@ -3,34 +3,27 @@
  */
 
 #include "zbuild.h"
+#include "zmemory.h"
 
 #ifdef X86_SSE2
 #include <immintrin.h>
 
 typedef __m128i chunk_t;
 
-#define CHUNK_SIZE 16
-
 #define HAVE_CHUNKMEMSET_2
 #define HAVE_CHUNKMEMSET_4
 #define HAVE_CHUNKMEMSET_8
 
 static inline void chunkmemset_2(uint8_t *from, chunk_t *chunk) {
-    int16_t tmp;
-    zmemcpy_2(&tmp, from);
-    *chunk = _mm_set1_epi16(tmp);
+    *chunk = _mm_set1_epi16(zng_memread_2(from));
 }
 
 static inline void chunkmemset_4(uint8_t *from, chunk_t *chunk) {
-    int32_t tmp;
-    zmemcpy_4(&tmp, from);
-    *chunk = _mm_set1_epi32(tmp);
+    *chunk = _mm_set1_epi32(zng_memread_4(from));
 }
 
 static inline void chunkmemset_8(uint8_t *from, chunk_t *chunk) {
-    int64_t tmp;
-    zmemcpy_8(&tmp, from);
-    *chunk = _mm_set1_epi64x(tmp);
+    *chunk = _mm_set1_epi64x(zng_memread_8(from));
 }
 
 static inline void loadchunk(uint8_t const *s, chunk_t *chunk) {
@@ -43,11 +36,14 @@ static inline void storechunk(uint8_t *out, chunk_t *chunk) {
 
 #define CHUNKSIZE        chunksize_sse2
 #define CHUNKCOPY        chunkcopy_sse2
-#define CHUNKCOPY_SAFE   chunkcopy_safe_sse2
 #define CHUNKUNROLL      chunkunroll_sse2
 #define CHUNKMEMSET      chunkmemset_sse2
 #define CHUNKMEMSET_SAFE chunkmemset_safe_sse2
 
 #include "chunkset_tpl.h"
+
+#define INFLATE_FAST     inflate_fast_sse2
+
+#include "inffast_tpl.h"
 
 #endif
