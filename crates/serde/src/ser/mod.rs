2449c9c93e139010d3c581555ac2b9ac3393f2e3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-03-11 07:45:26 -0700

    Revert "Update serde to 1.0.218"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/502ee89d-e85b-4539-b2b4-ff5f26cd633b). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ia16e232c254dbf58c0dc3a25714e3da7ecde1391
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- crates/serde/src/ser/mod.rs
+++ crates/serde/src/ser/mod.rs
@@ -97,8 +97,8 @@
 //!
 //! [Implementing `Serialize`]: https://serde.rs/impl-serialize.html
 //! [`LinkedHashMap<K, V>`]: https://docs.rs/linked-hash-map/*/linked_hash_map/struct.LinkedHashMap.html
-//! [`Serialize`]: crate::Serialize
-//! [`Serializer`]: crate::Serializer
+//! [`Serialize`]: ../trait.Serialize.html
+//! [`Serializer`]: ../trait.Serializer.html
 //! [`postcard`]: https://github.com/jamesmunns/postcard
 //! [`linked-hash-map`]: https://crates.io/crates/linked-hash-map
 //! [`serde_derive`]: https://crates.io/crates/serde_derive
@@ -173,8 +173,8 @@ macro_rules! declare_error_trait {
             /// }
             /// ```
             ///
-            /// [`Path`]: std::path::Path
-            /// [`Serialize`]: crate::Serialize
+            /// [`Path`]: https://doc.rust-lang.org/std/path/struct.Path.html
+            /// [`Serialize`]: ../trait.Serialize.html
             fn custom<T>(msg: T) -> Self
             where
                 T: Display;
@@ -345,7 +345,7 @@ pub trait Serializer: Sized {
     /// in-memory data structures may be simplified by using `Ok` to propagate
     /// the data structure around.
     ///
-    /// [`io::Write`]: std::io::Write
+    /// [`io::Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
     type Ok;
 
     /// The error type when some error occurs during serialization.
@@ -769,7 +769,7 @@ pub trait Serializer: Sized {
     /// # fn main() {}
     /// ```
     ///
-    /// [`None`]: core::option::Option::None
+    /// [`None`]: https://doc.rust-lang.org/std/option/enum.Option.html#variant.None
     fn serialize_none(self) -> Result<Self::Ok, Self::Error>;
 
     /// Serialize a [`Some(T)`] value.
@@ -802,7 +802,7 @@ pub trait Serializer: Sized {
     /// # fn main() {}
     /// ```
     ///
-    /// [`Some(T)`]: core::option::Option::Some
+    /// [`Some(T)`]: https://doc.rust-lang.org/std/option/enum.Option.html#variant.Some
     fn serialize_some<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + Serialize;
@@ -1353,7 +1353,8 @@ pub trait Serializer: Sized {
     /// }
     /// ```
     ///
-    /// [`serialize_str`]: Self::serialize_str
+    /// [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
+    /// [`serialize_str`]: #tymethod.serialize_str
     #[cfg(any(feature = "std", feature = "alloc"))]
     fn collect_str<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
@@ -1804,9 +1805,9 @@ pub trait SerializeMap {
     /// care about performance or are not able to optimize `serialize_entry` any
     /// better than this.
     ///
-    /// [`Serialize`]: crate::Serialize
-    /// [`serialize_key`]: Self::serialize_key
-    /// [`serialize_value`]: Self::serialize_value
+    /// [`Serialize`]: ../trait.Serialize.html
+    /// [`serialize_key`]: #tymethod.serialize_key
+    /// [`serialize_value`]: #tymethod.serialize_value
     fn serialize_entry<K, V>(&mut self, key: &K, value: &V) -> Result<(), Self::Error>
     where
         K: ?Sized + Serialize,
